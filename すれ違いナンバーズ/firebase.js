2b035db4b29cd8f574bb6cbb7862f43780ab0db7
aritaku-qn0 <aritakusecond@gmail.com>
2026-06-30 02:28:52 +0900

    googleログイン関係のやつ2

---
+++ すれ違いナンバーズ/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDHkY0j2G0jGyimRTrUr3CpArDHc5hQ98",
+  authDomain: "surechigai-numbers.firebaseapp.com",
+  projectId: "surechigai-numbers",
+  storageBucket: "surechigai-numbers.firebasestorage.app",
+  messagingSenderId: "1049904814367",
+  appId: "1:1049904814367:web:54203af0eff830c4a9382a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+window.auth = getAuth(app);
+window.db = getFirestore(app);
+window.provider = new GoogleAuthProvider();
+
+window.signInWithPopup = signInWithPopup;
+window.signOut = signOut;
+window.onAuthStateChanged = onAuthStateChanged;
+window.doc = doc;
+window.getDoc = getDoc;
+window.setDoc = setDoc;
